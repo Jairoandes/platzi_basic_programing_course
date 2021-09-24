42ca3f6e4bd5f52d23c6353fae76998f1f42e71c
jairoandes <jairoandes@gmail.com>
2021-09-23 22:24:01 -0500

    Cambios con varias funciones y agregando una variable de estado adicional

--- Programacion_Basica/mouse/eventos_mouse.js
+++ Programacion_Basica/mouse/eventos_mouse.js
@@ -1,18 +1,24 @@
 var cuadrito = document.getElementById("area_de_dibujo");
-document.addEventListener("mousedown", dibujarMouse);
-document.addEventListener("mouseup", dibujarMouse);
+document.addEventListener("mousedown", bajarPincel);
+document.addEventListener("mouseup", levantarPincel);
 document.addEventListener("mousemove", dibujarMouse);
 var papel = cuadrito.getContext("2d");
+var estado = 0;
 
 function dibujarPunto(color, xinicial, yinicial, xfinal, yfinal, lienzo)
 {
     lienzo.beginPath();
     lienzo.strokeStyle = color;
-    lienzo.lineWidth = 3;
+    lienzo.lineWidth = 4;
     lienzo.moveTo(xinicial, yinicial);
     lienzo.lineTo(xfinal, yfinal);
     lienzo.stroke();
     lienzo.closePath();
+}
+
+function bajarPincel(evento)
+{
+    estado = 1;
 }
 
 function dibujarMouse(evento)
@@ -20,20 +26,13 @@ function dibujarMouse(evento)
     var  colorcito = "blue";
     var x = evento.x;
     var y = evento.y;
-    console.log(x);
-    console.log(y);
-    if(evento.type == "mousedown")
+    if(estado == 1)
         {
-            console.log("Pincel abajo!");
             dibujarPunto(colorcito, x-1, y-1, x+1, y+1, papel);
-            if(evento.type == "mousemove")
-                {
-                    console.log("A pintar!");
-                    //dibujarPunto("red", x-1, y-1, x+1, y+1, papel);
-                }
         }
-    if(evento.type == "mouseup")
-        {
-            console.log("Hasta ahì fue!");
-        }
+}
+
+function levantarPincel(evento)
+{
+    estado = 0;
 }
